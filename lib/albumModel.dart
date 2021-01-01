e598b3aabd917e3d9bf58f8bc901b9496ffa2f4a
mocha234 <64956241+mocha234@users.noreply.github.com>
2021-01-02 01:05:29 +0800

    workingg

--- lib/albumModel.dart
+++ lib/albumModel.dart
@@ -1,23 +1,21 @@
-<<<<<<< HEAD
 class Album {
   // final List tagId;
   //final String tagName;
-  
+
   final String id;
   final String project;
   final String iteration;
   final String created;
   final List<dynamic> predictions;
 
-  Album({this.id, this.project, this.iteration, this.created, this.predictions});
+  Album(
+      {this.id, this.project, this.iteration, this.created, this.predictions});
 
   factory Album.fromJson(Map<String, dynamic> json) {
     var list = json['predictions'] as List;
     print(list.runtimeType); //returns List<dynamic>
     List<Predictions> predictionList =
         list.map((i) => Predictions.fromJson(i)).toList();
-
-   
 
     return Album(
       id: json['id'],
@@ -26,7 +24,6 @@ class Album {
       created: json['created'],
       predictions: predictionList,
     );
-
   }
 }
 
@@ -45,51 +42,3 @@ class Predictions {
 }
 
 //Ref: https://medium.com/flutter-community/parsing-complex-json-in-flutter-747c46655f51
-=======
-class Album {
-  // final List tagId;
-  //final String tagName;
-  
-  final String id;
-  final String project;
-  final String iteration;
-  final String created;
-  final List<dynamic> predictions;
-
-  Album({this.id, this.project, this.iteration, this.created, this.predictions});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    var list = json['predictions'] as List;
-    print(list.runtimeType); //returns List<dynamic>
-    List<Predictions> predictionList =
-        list.map((i) => Predictions.fromJson(i)).toList();
-
-   
-
-    return Album(
-      id: json['id'],
-      project: json['project'],
-      iteration: json['iteration'],
-      created: json['created'],
-      predictions: predictionList,
-    );
-
-  }
-}
-
-class Predictions {
-  final double probability;
-  final String tagId;
-  final String tagName;
-  Predictions({this.probability, this.tagId, this.tagName});
-
-  factory Predictions.fromJson(Map<String, dynamic> json) {
-    return Predictions(
-        probability: json['probability'],
-        tagId: json['tagId'],
-        tagName: json['tagName']);
-  }
-}
-
-//Ref: https://medium.com/flutter-community/parsing-complex-json-in-flutter-747c46655f51
->>>>>>> d769e6192faaf7475901cf55f3818566b9b9d4fe
