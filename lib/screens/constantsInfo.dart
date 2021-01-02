e1dba422ba0b425aa119d1d0ff2dec909ad37095
mocha234 <nelsonwongisme@gmail.com>
2021-01-02 22:45:58 +0800

    added on guide on indexex

--- lib/screens/constantsInfo.dart
+++ lib/screens/constantsInfo.dart
@@ -11,6 +11,7 @@ const Map<String, int> diseaseIndexMapping = {
 };
 
 const Map<int, Map<String, String>> projectAPI = {
+  //Index must match categoriesMap
   0: {
     "APIEndpoint":
         "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/c1e64998-1818-447c-aaaf-c843556c689d/classify/iterations/Iteration1/image",
